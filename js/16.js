00cf7c29853682fcf6945d182e84d94ffd27e79b
Alan Shalem <63003387+alanshalem@users.noreply.github.com>
2022-06-26 19:36:27 -0300

    add metodos de ordenamiento

--- js/16.js
+++ js/16.js
@@ -31,3 +31,39 @@ tecnologias.forEach((tech, index) => console.log(tech, index));
 const arrayMap = tecnologias.map((tech) => tech);
 
 console.log(arrayMap);
+
+// Metodos de Ordenamiento
+
+// JavaScript Sort Array of Numbers
+let values = [1, 2, 8, 9, 3];
+const sorted_values = values.sort();
+console.log(values); // Modifica el array original
+console.log(sorted_values);
+
+// JavaScript Sort Array of Strings
+let array_students_str = ['Alex', 'Cathy', 'Lincoln', 'Jeff'];
+const sorted_array_students_str = array_students_str.sort();
+console.log(array_students_str); // Modifica el array original
+console.log(sorted_array_students_str);
+
+// JavaScript Sort Array of Objects
+let array_students_obj = [
+  { name: 'Alex', age: 16 },
+  { name: 'Cathy', age: 14 },
+  { name: 'Lincoln', age: 14 },
+  { name: 'Jeff', age: 15 },
+];
+
+const sorted_array_students_obj = array_students_obj.sort(function (a, b) {
+  return a.age - b.age;
+});
+console.log(array_students_obj);
+console.log(sorted_array_students_obj);
+
+// JavaScript Sort in Reverse Order
+
+let students = ['Alex', 'Cathy', 'Lincoln', 'Jeff'];
+const reversed_students = students.reverse();
+console.log(students);
+console.log(reversed_students);
+
